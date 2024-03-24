5f5a22dd1f6c77ff5d94d740933b4b32b4630923
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-24 22:15:24 +0600

    update some static files

--- ecommerceDRF/settings.py
+++ ecommerceDRF/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
